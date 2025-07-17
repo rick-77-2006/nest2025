59c373eb660db265dc2fd6035e138ab78ea511b9
Rick <46992687+rick-77-2006@users.noreply.github.com>
2025-07-17 19:10:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ buttonAdd.addEventListener("click", () => {
         ul.appendChild(li);
         form.elements[`nameValue`].value = ''
     } else {
-        alert(`Error! Input data in format Name=Value`)
+        alert(`Error! Input data in format Name=Value NO Cyrillic or special characters`)
     }
 });
 
